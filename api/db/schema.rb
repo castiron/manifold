ec8ee7e96dfbca5eb17f8d487ab9f46788d0880d
Zach Davis <zach@castironcoding.com>
2015-11-23 08:31:37 -0800

    [F] Add project model and controller to API

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109192408) do
+ActiveRecord::Schema.define(version: 20151120195625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20151109192408) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "position"
+    t.integer  "project_id"
   end
 
   create_table "ingestion_sources", force: :cascade do |t|
@@ -45,15 +46,16 @@ ActiveRecord::Schema.define(version: 20151109192408) do
   create_table "projects", force: :cascade do |t|
     t.string   "title"
     t.string   "subtitle"
-    t.boolean  "published"
+    t.boolean  "published",          default: false, null: false
     t.datetime "published_datetime"
     t.text     "description"
     t.string   "cover_file_name"
     t.string   "cover_content_type"
     t.integer  "cover_file_size"
     t.datetime "cover_updated_at"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.boolean  "featured",           default: false
   end
 
   create_table "resources", force: :cascade do |t|
@@ -115,6 +117,7 @@ ActiveRecord::Schema.define(version: 20151109192408) do
     t.text     "page_list"
     t.text     "landmarks"
     t.text     "structure_titles"
+    t.integer  "project_id"
   end
 
 end
